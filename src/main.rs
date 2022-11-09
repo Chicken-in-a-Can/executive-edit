9b36eb3dc1d887bd862ed0029823cf342600f973
Chicken-in-a-Can <64804772+Chicken-in-a-Can@users.noreply.github.com>
2022-11-09 10:42:59 -0600

    now sets cursor, kinda

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::{io, thread, time::Duration, process::exit, fs, env};
+use std::{io, io::Write, thread, time::Duration, process::exit, fs, env};
 use tui::{
     backend::CrosstermBackend,
-    widgets::{Widget, Block, Borders},
-    layout::{Layout, Constraint, Direction},
-    Terminal
+    widgets::{Widget, Block, Borders, Paragraph, Wrap},
+    style::{Style, Color},
+    layout::{Layout, Constraint, Direction, Alignment},
+    Terminal,
+    text::{Span, Spans}
 };
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers, KeyEvent, read},
@@ -19,20 +21,38 @@ fn main() -> Result<(), io::Error> {
         exit(1);
     }
     let file_path = &args[1];
+    let file_path_span = Span::raw(String::from(file_path));
     let file_string = fs::read_to_string(file_path).expect("File not able to be read");
+    let mut file_vector: Vec<&str> = file_string.lines().collect();
+    let mut line_count: u16 = file_vector.len() as u16;
+    let mut line_lengths: Vec<u16> = Vec::new();
+    for i in 0..line_count{
+        line_lengths.push(file_vector[i as usize].len() as u16);
+    }
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let mut display_text = vec![Spans::from(file_vector[0])];
+    for i in 1..line_count{
+        display_text.push(Spans::from(file_vector[i as usize]));
+    }
+
     terminal.draw(|f| {
         let size = f.size();
         let block = Block::default()
-            .title("Executive Text Editor")
+            .title(file_path_span)
             .borders(Borders::ALL);
+        let paragraph = Paragraph::new(display_text.clone())
+            .style(Style::default())
+            .alignment(Alignment::Left)
+            .wrap(Wrap {trim: true });
         f.render_widget(block, size);
     })?;
+    terminal.set_cursor(*line_lengths.last().unwrap(),line_count);
+    terminal.show_cursor();
 
     loop{
         match read().unwrap() {
@@ -41,6 +61,26 @@ fn main() -> Result<(), io::Error> {
                 modifiers: KeyModifiers::CONTROL,
                 ..}
             ) => break,
+            Event::Key(KeyEvent {
+                code: KeyCode::Up,
+                modifiers: KeyModifiers::NONE,
+                ..}
+            ) => cursorMove(&mut terminal, 'n'),
+            Event::Key(KeyEvent {
+                code: KeyCode::Down,
+                modifiers: KeyModifiers::NONE,
+                ..}
+            ) => cursorMove(&mut terminal, 's'),
+            Event::Key(KeyEvent {
+                code: KeyCode::Right,
+                modifiers: KeyModifiers::NONE,
+                ..}
+            ) => cursorMove(&mut terminal, 'e'),
+            Event::Key(KeyEvent {
+                code: KeyCode::Left,
+                modifiers: KeyModifiers::NONE,
+                ..}
+            ) => cursorMove(&mut terminal, 'w'),
             _ => (),
         }
     }
@@ -55,4 +95,18 @@ fn main() -> Result<(), io::Error> {
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+fn cursorMove<B: tui::backend::Backend>(terminal: &mut Terminal<B>, mut direction: char){
+    let mut x_pos = terminal.get_cursor().unwrap().0;
+    let mut y_pos = terminal.get_cursor().unwrap().1;
+    if x_pos == 0 || x_pos == 1 && direction == 'w'{direction = 'q'}
+    if y_pos == 0 || y_pos == 1 && direction == 'n'{direction = 'q'}
+    match direction{
+        'n' => terminal.set_cursor(x_pos, y_pos - 1),
+        's' => terminal.set_cursor(x_pos, y_pos + 1),
+        'e' => terminal.set_cursor(x_pos + 1, y_pos),
+        'w' => terminal.set_cursor(x_pos - 1, y_pos),
+        _ => Ok(()),
+    };
 }
