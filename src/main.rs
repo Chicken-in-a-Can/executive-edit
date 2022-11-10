602035bfb2bd82009b9d1e9ef6f7de2db3a1a1d9
Chicken-in-a-Can <ben.stidham1@gmail.com>
2022-11-09 22:46:16 -0600

    Can open files good now, cursor moves good

--- src/main.rs
+++ src/main.rs
@@ -24,24 +24,22 @@ fn main() -> Result<(), io::Error> {
     let file_path_span = Span::raw(String::from(file_path));
     let file_string = fs::read_to_string(file_path).expect("File not able to be read");
     let mut file_vector: Vec<&str> = file_string.lines().collect();
-    let mut line_count: u16 = file_vector.len() as u16;
+    file_vector.push("");
     let mut line_lengths: Vec<u16> = Vec::new();
-    for i in 0..line_count{
+    for i in 0..file_vector.len(){
         line_lengths.push(file_vector[i as usize].len() as u16);
     }
+
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let mut display_text = vec![Spans::from(file_vector[0])];
-    for i in 1..line_count{
-        display_text.push(Spans::from(file_vector[i as usize]));
-    }
+    let display_text = str_vec_to_span(file_vector.clone());
 
-    terminal.draw(|f| {
-        let size = f.size();
+    terminal.draw(|main| {
+        let mut size = main.size();
         let block = Block::default()
             .title(file_path_span)
             .borders(Borders::ALL);
@@ -49,9 +47,14 @@ fn main() -> Result<(), io::Error> {
             .style(Style::default())
             .alignment(Alignment::Left)
             .wrap(Wrap {trim: true });
-        f.render_widget(block, size);
+        main.render_widget(block, size);
+        size.x += 1;
+        size.y += 1;
+        size.width -= 2;
+        size.height -= 2;
+        main.render_widget(paragraph, size);
     })?;
-    terminal.set_cursor(*line_lengths.last().unwrap(),line_count);
+    terminal.set_cursor(1, file_vector.len() as u16);
     terminal.show_cursor();
 
     loop{
@@ -65,22 +68,22 @@ fn main() -> Result<(), io::Error> {
                 code: KeyCode::Up,
                 modifiers: KeyModifiers::NONE,
                 ..}
-            ) => cursorMove(&mut terminal, 'n'),
+            ) => cursor_move(&mut terminal, line_lengths.clone(), 'n'),
             Event::Key(KeyEvent {
                 code: KeyCode::Down,
                 modifiers: KeyModifiers::NONE,
                 ..}
-            ) => cursorMove(&mut terminal, 's'),
+            ) => cursor_move(&mut terminal, line_lengths.clone(), 's'),
             Event::Key(KeyEvent {
                 code: KeyCode::Right,
                 modifiers: KeyModifiers::NONE,
                 ..}
-            ) => cursorMove(&mut terminal, 'e'),
+            ) => cursor_move(&mut terminal, line_lengths.clone(), 'e'),
             Event::Key(KeyEvent {
                 code: KeyCode::Left,
                 modifiers: KeyModifiers::NONE,
                 ..}
-            ) => cursorMove(&mut terminal, 'w'),
+            ) => cursor_move(&mut terminal, line_lengths.clone(), 'w'),
             _ => (),
         }
     }
@@ -97,16 +100,30 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn cursorMove<B: tui::backend::Backend>(terminal: &mut Terminal<B>, mut direction: char){
+fn cursor_move<B: tui::backend::Backend>(terminal: &mut Terminal<B>, line_lengths: Vec<u16>, mut direction: char){
     let mut x_pos = terminal.get_cursor().unwrap().0;
     let mut y_pos = terminal.get_cursor().unwrap().1;
     if x_pos == 0 || x_pos == 1 && direction == 'w'{direction = 'q'}
     if y_pos == 0 || y_pos == 1 && direction == 'n'{direction = 'q'}
     match direction{
-        'n' => terminal.set_cursor(x_pos, y_pos - 1),
-        's' => terminal.set_cursor(x_pos, y_pos + 1),
-        'e' => terminal.set_cursor(x_pos + 1, y_pos),
-        'w' => terminal.set_cursor(x_pos - 1, y_pos),
-        _ => Ok(()),
-    };
+        'n' => y_pos -= 1,
+        's' => y_pos += 1,
+        'e' => x_pos += 1,
+        'w' => x_pos -= 1,
+        _ => (),
+    }
+    if y_pos > (line_lengths.len()) as u16{
+        y_pos = (line_lengths.len()) as u16;
+    }
+    if x_pos > line_lengths[(y_pos - 1) as usize] + 1{
+        x_pos = line_lengths[(y_pos - 1) as usize] + 1;
+    }
+    terminal.set_cursor(x_pos, y_pos);
+}
+fn str_vec_to_span(file_vector: Vec<&str>) -> Vec<Spans<'_>>{
+    let mut display_text = vec![Spans::from(file_vector[0])];
+    for i in 1..file_vector.len(){
+        display_text.push(Spans::from(file_vector[i as usize]));
+    }
+    return display_text
 }
